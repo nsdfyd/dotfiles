497284da3cd8b61caa734de8f78c5f367e911aaf
Aleksandr Ivanov <aleivanov@x5.ru>
2021-08-31 13:18:57 +0300

    Vimwiki configuration

--- nvim/.config/nvim/lua/conf-vimwiki/init.lua
+++ nvim/.config/nvim/lua/conf-vimwiki/init.lua
@@ -1,3 +1,4 @@
 vim.cmd[[
-let g:vimwiki_list = [{'path': '~/Code/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
+let g:vimwiki_list = [{'path': '~/Code/docs/vimwiki/', 'syntax': 'markdown', 'ext': '.md', 'links_space_char': '_'}]
 ]]
+vim.g.vimwiki_markdown_link_ext = 1
